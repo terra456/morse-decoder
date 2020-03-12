31d3f2a3fab2d8d4e109120dc927550ff03b3478
Olga Bainova (nb) <terra.456@yandex.ru>
2020-03-12 19:40:23 +0500

    feat: make morze-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    var arr = Array.from(expr);
+    
+    var finishArr = [];
+
+    for (let i = 0; i < arr.length; i=i+10) {
+      var decSymbArr = arr.slice(i, i+10);
+
+      if (decSymbArr[0] == "*") {
+          finishArr.push(" ");
+      } else {
+        var dotWord = "";
+          for (let j = 0; j < decSymbArr.length; j = j+2) {
+            const twoSymb = decSymbArr[j] + decSymbArr[j+1];
+            var obj = {
+              "00": "",
+              "10": ".",
+              "11": "-",
+            };
+
+            dotWord = dotWord + obj[twoSymb];
+          }
+
+          finishArr.push(MORSE_TABLE[dotWord]);
+        }
+    }
+      return finishArr.join("");
 }
 
 module.exports = {
